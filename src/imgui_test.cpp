92e1b2998f825b07b1dac2142f96a38c5269d475
uzo <uzoochogu@yahoo.com>
2023-09-02 02:26:35 +0100

    Committing Imgui demo, app and test code + CMakeLists.txt file for project

---
+++ src/imgui_test.cpp
@@ -0,0 +1,96 @@
+// imGUIexample.cpp : Defines the entry point for the application.
+//
+
+#include "UseImGui.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+
+class CustomImGui : public UseImGui {
+public:
+	virtual void Update() override {
+
+		// render your GUI
+		static float f = 0.0f;
+		static int counter = 0;
+
+		ImGui::Begin("CustomImGui Hello, world!");              // Create a window called "Hello, world!" and append into it.
+
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+		bool clear_color_changed = ImGui::ColorEdit3("clear color", (float*)clear_color); // Edit 3 floats representing a color
+
+		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			counter++;
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+		ImGui::End();
+
+
+
+		if (clear_color_changed) {
+			change_clear_color(clear_color[0], clear_color[1], clear_color[2]);
+		}
+
+	}
+
+private:
+	float clear_color[3] = { .0f, .0f, .0f };
+	void change_clear_color(float r, float g, float b) {
+		glClearColor(r, g, b, 1.00f);
+	}
+};
+
+
+
+int main()
+{
+	// Setup window
+	if (!glfwInit())
+		return 1;
+
+	// GL 3.0 + GLSL 130
+	const char *glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+
+	// Create window with graphics context
+	GLFWwindow *window = glfwCreateWindow(1280, 720, "Dear ImGui - Example", NULL, NULL);
+	if (window == NULL)
+		return 1;
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))  // tie window context to glad's opengl funcs
+		throw("Unable to context to OpenGL");
+
+	int screen_width, screen_height;
+	glfwGetFramebufferSize(window, &screen_width, &screen_height);
+	glViewport(0, 0, screen_width, screen_height);
+
+	CustomImGui myimgui;
+	myimgui.Init(window, glsl_version);
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		myimgui.NewFrame();
+		myimgui.Update();
+		myimgui.Render();
+		glfwSwapBuffers(window);
+
+	}
+	myimgui.Shutdown();
+
+	return 0;
+}
